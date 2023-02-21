51190489dd7e4cf301ea1fa62a6399316925d6f5
suzhenyi <suzhenyi1999@163.com>
2023-02-21 21:33:27 +0800

    feat: sectionItem

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -5,11 +5,17 @@ const RoomItem = memo((props) => {
   const { itemData } = props
 
   return (
-    <ItemWrapper>
+    <ItemWrapper verifyColor={itemData?.verify_info?.text_color || '#39576a'}>
       <div className='inner'>
         <div className='cover'>
           <img src={itemData.picture_url} alt="" />
         </div>
+        <div className='desc'>
+          {itemData.verify_info.messages.join(" · ")}
+        </div>
+        <div className='name'>{itemData.name}</div>
+        <div className='price'>¥{itemData.price}/晚</div>
+
       </div>
     </ItemWrapper>
   )
