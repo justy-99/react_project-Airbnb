c68812145acd5e4a94206ba77ace079b77ebc3a1
suzhenyi <suzhenyi1999@163.com>
2023-02-24 21:18:30 +0800

    section-tabs

---
+++ src/components/section-tabs/index.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+import { TabsWrapper } from './style'
+
+const SectionTabs = memo((props) => {
+  const { tabNames } = props
+
+  return (
+    <TabsWrapper>
+      {tabNames}
+    </TabsWrapper>
+  )
+})
+
+SectionTabs.propTypes = {
+  tabNames: PropTypes.array
+}
+
+export default SectionTabs
--- src/views/home/c-cpns/home-section-v2/index.jsx
+++ src/views/home/c-cpns/home-section-v2/index.jsx
@@ -3,14 +3,18 @@ import React, { memo } from 'react'
 import { SectionV2Wrapper } from './style'
 import SectionHeader from '@/components/section-header'
 import SectionRooms from '@/components/section-rooms'
+import SectionTabs from '@/components/section-tabs'
 
 const HomeSectionV2 = memo((props) => {
 
   const { infoData } = props
 
+  const tabNames = infoData.dest_address?.map(item => item.name)
+
   return (
     <SectionV2Wrapper>
       <SectionHeader title={infoData.title} subtitle= {infoData.subtitle}/>
+      <SectionTabs tabNames={tabNames}/>
       <SectionRooms roomList={infoData.list} itemWidth="33.33%"/>
     </SectionV2Wrapper>
   )
