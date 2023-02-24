9eb8509010f33e93fe70d4d14bbb1b89d2bc6e6b
suzhenyi <suzhenyi1999@163.com>
2023-02-24 21:19:09 +0800

    section-tabs

--- src/components/section-tabs/index.jsx
+++ src/components/section-tabs/index.jsx
@@ -3,7 +3,7 @@ import React, { memo } from 'react'
 import { TabsWrapper } from './style'
 
 const SectionTabs = memo((props) => {
-  const { tabNames } = props
+  const { tabNames = [] } = props
 
   return (
     <TabsWrapper>
